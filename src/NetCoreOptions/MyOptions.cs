78a9d614774e55d7cb37819fcb743b5502992b6b
Neil Dobson <neil.dobson71@gmail.com>
2021-06-24 16:12:03 +1000

    Add example

---
+++ src/NetCoreOptions/MyOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreOptions
+{
+    public class MyOptions
+    {
+        public const string Path = "MyOptions";
+        
+        public decimal[] MyValues {get;set;}
+    }
+}
---
+++ src/NetCoreOptions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace NetCoreOptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config =
+                new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", false, true)
+                    .Build();
+            
+            var myOptions = config.GetSection(MyOptions.Path)
+                .Get<MyOptions>();
+            
+            Console.WriteLine($"Count: {myOptions.MyValues.Length}");
+            foreach (var myValue in myOptions.MyValues)
+            {
+                Console.WriteLine($"Value: {myValue}");
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
